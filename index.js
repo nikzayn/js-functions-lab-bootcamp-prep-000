931a9e664313ecc4d72ec7edbfa480be8812695a
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-01-28 19:22:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return 'Happy holidays, ${name}!'
 }
-function happyHolidaysTo(name, holiday){
+function happyHolidaysTo(name, Diwali){
   return 'Happy ${holiday}, ${name}'
 }
