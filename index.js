d901ac451c80717a4c70ac5f6154bd8c6b3b9bad
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-01-28 19:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(Christmas, Angelina){
   return `Happy ${Christmas}, ${Angelina}!`
 }
 function holidayCountdown(Christmas, twenty){
-  `It's ${20} days until ${Christmas}!`
+  `It's ${twenty} days until ${Christmas}!`
 }
